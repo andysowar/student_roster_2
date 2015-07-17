72987817655eb5dfaf070916f0f163b04397bad8
Andy Sowar <andy.sowar@gmail.com>
2015-07-16 18:01:13 -0600

    initial commit

---
+++ db/migrate/20150716235834_create_students.rb
@@ -0,0 +1,15 @@
+class CreateStudents < ActiveRecord::Migration
+  def change
+    create_table :students do |t|
+      t.string :first_name
+      t.string :last_name
+      t.string :email
+      t.string :address
+      t.string :cell_phone
+      t.string :home_phone
+      t.string :work_phone
+
+      t.timestamps null: false
+    end
+  end
+end
