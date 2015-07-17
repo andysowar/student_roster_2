b82b01ad408cad940f0ec8fa571666c1d0d96e0d
Andy Sowar <andy.sowar@gmail.com>
2015-07-16 21:19:12 -0600

    Added format regex for phone & email
    [#99272642]

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,9 +1,12 @@
 class Student < ActiveRecord::Base
+  PHONE_REGEX = /\A\(?([0-9]{3})\)?[-.●]?([0-9]{3})[-.●]?([0-9]{4})\z/i
+  EMAIL_REGEX = /\A([\w\.%\+\-]+)@([\w\-]+\.)+([\w]{2,})\z/i
+
   validates :first_name, presence: true, length: { maximum:50 }
   validates :last_name, presence: true
   validates :address , presence: true
-  validates :email, presence: true, uniqueness: true
-  validates :cell_phone, presence: true
-  validates :work_phone, presence: true
-  validates :home_phone, presence: true
+  validates :email, presence: true, uniqueness: true, format: { with: EMAIL_REGEX }
+  validates :cell_phone, presence: true, format: { with: PHONE_REGEX }
+  validates :work_phone, presence: true, format: { with: PHONE_REGEX }
+  validates :home_phone, presence: true, format: { with: PHONE_REGEX }
 end
