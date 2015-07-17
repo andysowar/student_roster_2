3bf090f285878a3b69038c514169a6107225e613
Andy Sowar <andy.sowar@gmail.com>
2015-07-16 20:00:09 -0600

    First version validating presence
    [#99272642]

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,2 +1,9 @@
 class Student < ActiveRecord::Base
+  validates :first_name, presence: true, length: { maximum:50 }
+  validates :last_name, presence: true
+  validates :address , presence: true
+  validates :email, presence: true, uniqueness: true
+  validates :cell_phone, presence: true
+  validates :work_phone, presence: true
+  validates :home_phone, presence: true
 end
